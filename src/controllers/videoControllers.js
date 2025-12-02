e9f50149d88e6accf5f6faba525741e879f9bbdf
nakseongchoi <nakseongchoi@gmail.com>
2025-12-02 16:47:17 +0900

    Update videoControllers.js

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,16 +1,13 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
-  console.log(req.params);
   res.send(`See Video #${req.params.id}`);
 };
 export const search = (req, res) => res.send("Search Videos");
 export const edit = (req, res) => {
-  console.log(req.params);
   res.send(`Edit Video #${req.params.id}`);
 };
 
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
   res.send(`Delete Video #${req.params.id}`);
 };
 export const upload = (req, res) => res.send("Upload Video");
