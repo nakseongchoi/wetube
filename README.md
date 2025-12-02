24dc4a65a11a54f9c622d91cb23e12968debad51
nakseongchoi <nakseongchoi@gmail.com>
2025-12-02 16:15:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ -> Trending
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user
+/users/logout -> Log out
+/users/edit -> Edit my profile
+/users/delete -> Delete my profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+console.log("CWD:", process.cwd());
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
