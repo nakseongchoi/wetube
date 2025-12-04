a7fc8f9ddf06109202476135f468a7dfa8d14ccb
nakseongchoi <nakseongchoi@gmail.com>
2025-12-04 14:32:29 +0900

    6.11

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,47 +1,49 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createAt: "2minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createAt: "2minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createAt: "2minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+// 이전 수업용 fake data >> 따라서 아래 있었던 video 관련한 것 다 삭제
+// let videos = [
+//   {
+//     title: "First Video",
+//     rating: 5,
+//     comments: 2,
+//     createAt: "2minutes ago",
+//     views: 1,
+//     id: 1,
+//   },
+//   {
+//     title: "Second Video",
+//     rating: 5,
+//     comments: 2,
+//     createAt: "2minutes ago",
+//     views: 59,
+//     id: 2,
+//   },
+//   {
+//     title: "Third Video",
+//     rating: 5,
+//     comments: 2,
+//     createAt: "2minutes ago",
+//     views: 59,
+//     id: 3,
+//   },
+// ];
+
+import Video from "../models/video";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home" });
 };
 export const watch = (req, res) => {
   const id = req.params.id;
-  const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  res.render("watch", { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const id = req.params.id;
   const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const id = req.params.id;
   const title = req.body.title;
-  videos[id - 1].title = title;
   res.redirect(`/videos/${id}`);
 };
 
@@ -57,16 +59,6 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   //here we will add a video to the videos array
-  console.log(req.body);
   const title = req.body.title;
-  const newVideo = {
-    title: title,
-    rating: 0,
-    comments: 0,
-    createAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 console.log("CWD:", process.cwd());
 const app = express();
 const logger = morgan("dev");
@@ -18,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
