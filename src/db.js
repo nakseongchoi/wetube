3f98535c86f3a2485c26a24f3e1ae8b333d87e40
nakseongchoi <nakseongchoi@gmail.com>
2025-12-04 11:12:05 +0900

    #6.8

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+
+// mongoose 통해서 mongoDB 연결 됐는지 확인하는 코드
+
+const db = mongoose.connection;
+
+db.on("error", (err) => console.log("❌ DB 연결 실패:", err));
+db.once("open", () => console.log("✅ DB 연결 성공!"));
