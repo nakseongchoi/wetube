9f91a81d402f1cc3ffaf51420be3c907af63df2c
nakseongchoi <nakseongchoi@gmail.com>
2025-12-14 19:36:21 +0900

    7.15

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+console.log(process.env.DB_URL, process.env.COOKIE_SECRET);
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -14,11 +16,13 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
